80a24af1475eabe6634fd54b71a8851e35e2372a
Haggai Kaunda <haggaikaunda@berkeley.edu>
2017-09-08 22:24:00 +0000

    completes defining constructor and getter and setter methods or instance variables

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -10,7 +10,12 @@ class HangpersonGame
   
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
   end
+  
+  attr_accessor :guesses, :word, :wrong_guesses
+  
 
   # You can test it by running $ bundle exec irb -I. -r app.rb
   # And then in the irb: irb(main):001:0> HangpersonGame.get_random_word
